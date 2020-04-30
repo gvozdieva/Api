ce7f2f46a0e0aa6fde8271eca6f949971d0c225e
Anastasiia Gvozdieva <nastiagvozdieva@gmail.com>
2020-04-30 18:03:40 +0300

    third commit

--- httpRoutes/index.js
+++ httpRoutes/index.js
@@ -1,22 +1,26 @@
 const express = require('express');
+
 const router = express.Router();
-const articleController = require('controllers/article');
+// const articleController = require('controllers/article');
 const uniqid = require('uniqid');
 
-const sessionStore = {}; 
+const sessionModel = require('models/session');
 
-router.get('/', (req, res, next) => {
-  // const sid = uniqid();
-  // console.log(sid);
-  if( !req.cookies.sid ) {
-    const sid = uniqid();
-    sessionStore[sid] = {};
-    sessionStore[sid].name = 'Valera';
+const sessionStore = {};
 
+router.get('/', async (req, res, next) => {
+  if (!req.cookies.sid) {
+    const model = new sessionModel();
+    const doc = model.save();
+    // const sid = uniqid();
+
+    // sessionStore[sid] = {};
+    // sessionStore[sid].name = 'Valera';
+    const sid = doc.id;
     res.cookie('sid', sid);
   }
 
-  const { sid } = req.cookies; 
+  const { sid } = req.cookies;
 
   const user = sessionStore[sid];
   console.log(user);
